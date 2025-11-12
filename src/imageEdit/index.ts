fe9e2f6e517d3f682b98fc5e2c3ef2273c05e2b2
An <nooks@qq.com>
2025-11-12 09:22:28 +0800

    🐛 Bugfix: Fix ES module import paths missing .js extension

--- src/imageEdit/index.ts
+++ src/imageEdit/index.ts
@@ -1,6 +1,6 @@
 // 统一导出图像编辑核心功能
-export * from "./models";
-export * from "./core";
+export * from "./models.js";
+export * from "./core.js";
 
 // 重新导出类型和函数，保持向后兼容
 export type { ImageEditModel } from "./models";
@@ -18,15 +18,15 @@ export {
 	getModelsByCapability,
 	getAllCapabilities,
 	getModelByName,
-} from "./models";
+} from "./models.js";
 
 export {
 	ImageEditCore,
 	createImageEditCore,
-} from "./core";
+} from "./core.js";
 
 // 导出状态查询功能
-export { createImageEditStatusCore, ImageEditStatusCore } from "./status";
+export { createImageEditStatusCore, ImageEditStatusCore } from "./status.js";
 export type {
 	ImageEditStatusRequest,
 	ImageEditStatusResult,
--- src/text2image/index.ts
+++ src/text2image/index.ts
@@ -1,7 +1,7 @@
 // 统一导出文本转图像核心功能
-export * from "./models";
-export * from "./types";
-export * from "./core";
+export * from "./models.js";
+export * from "./types.js";
+export * from "./core.js";
 
 // 重新导出类型和函数，保持向后兼容
 export type { ModelInfo } from "./models";
@@ -15,9 +15,9 @@ export type {
 	Text2ImageStatusRequest,
 } from "./types";
 
-export { text2imageModels } from "./models";
+export { text2imageModels } from "./models.js";
 
 export {
 	Text2ImageCore,
 	createText2ImageCore,
-} from "./core";
+} from "./core.js";
